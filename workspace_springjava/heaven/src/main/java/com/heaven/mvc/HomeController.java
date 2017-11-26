2e087726ba65e0d10483a151b61a98301364f8b4
James Park <twinspark81@gmail.com>
2017-11-26 22:30:48 +0900

    version 1

--- workspace_springjava/heaven/src/main/java/com/heaven/mvc/HomeController.java
+++ workspace_springjava/heaven/src/main/java/com/heaven/mvc/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -35,12 +34,6 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
-	}
-	
-	@RequestMapping(value = "/show")
-	@ResponseBody
-	public String show() {
-		return "<h1>show</h1>";
 	}
 	
 }
